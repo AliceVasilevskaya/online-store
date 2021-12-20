0a685cd902a99073053ac424799dcf216882fb37
Alisa <vasilevskaya.alice@gmail.com>
2021-12-20 22:06:03 +0200

    homework-{1}-{3}-{rename action types}

--- src/context/products/product-action-types.js
+++ src/context/products/product-action-types.js
@@ -1,5 +1,5 @@
-const setProducts = "online-store/productsPage/SET_PRODUCTS";
-const setProduct = "online-store/productsPage/SET_PRODUCT";
-const toggleIsFetching = "online-store/productsPage/TOGGLE_IS_FETCHING";
+const setProducts = "ONLINE-STORE/PRODUCTS_PAGE/SET_PRODUCTS";
+const setProduct = "ONLINE-STORE/PRODUCTS_PAGE/SET_PRODUCT";
+const toggleIsFetching = "ONLINE-STORE/PRODUCTS_PAGE/TOGGLE_IS_FETCHING";
 
 export { setProduct, setProducts, toggleIsFetching };
